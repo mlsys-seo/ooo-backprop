053710cc3eb9659b7970b59593a72a57a8d8a69e
hyungjunoh <92hyungjun@gmail.com>
2022-01-29 23:51:00 +0900

    delete docker run

--- docker_run.sh
+++
@@ -1,7 +0,0 @@
-docker run -d -it \
-    -v ${PWD}:/workspace/bdsl \
-    --name sk-test \
-    --privileged --ipc=host --net=host \
-    --restart=always \
-    --gpus=all \
-    nvidia/cuda:11.1.1-cudnn8-devel-ubuntu18.04

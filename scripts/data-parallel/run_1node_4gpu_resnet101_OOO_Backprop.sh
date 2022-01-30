82ef6e0db9c2ba7118d1024c7c70a12098adac7e
sung.balance <sung.balance@gmail.com>
2022-01-30 14:37:46 +0900

    update

---
+++ scripts/data-parallel/run_1node_4gpu_resnet101_OOO_Backprop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# set up Model
+export MODEL_SIZE=101
+export BATCH_SIZE=96
+export REVERSE_FIRST_K=70
+export NUM_TRAINING_STEP=50
+
+# set up cluster
+export MASTER_HOST=127.0.0.1
+export MASTER_PORT=1234
+export NODE_HOST=127.0.0.1
+export NUM_NODE=1
+
+# set up cluster-setting
+export NUM_WORKER=4
+export NUM_SERVER=1
+
+export NUM_WORKER_PER_NODE=`expr $NUM_WORKER / $NUM_NODE`
+export NUM_SERVER_PER_NODE=`expr $NUM_SERVER / $NUM_NODE`
+
+export DEBUG_PRINT=0
+export DEBUG_C_PRINT=0
+
+export DOCKER_IMAGE="public.ecr.aws/bdsldocker/ooo_backprop_data_parallel:latest"
+
+../../expr/data-parallel/scripts/single_node/run.sh preset
---
+++ scripts/data-parallel/run_8node_32gpu_resnet101_BytePS.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# set up Model
+export MODEL_SIZE=101
+export BATCH_SIZE=96
+export REVERSE_FIRST_K=0
+export NUM_TRAINING_STEP=50
+
+# set up cluster
+export MASTER_HOST=127.0.0.1
+export MASTER_PORT=1234
+
+NODE_HOST_LIST=( )
+NODE_HOST_LIST[0]=NODE_IP
+NODE_HOST_LIST[1]=NODE_IP
+NODE_HOST_LIST[2]=NODE_IP
+NODE_HOST_LIST[3]=NODE_IP
+NODE_HOST_LIST[4]=NODE_IP
+NODE_HOST_LIST[5]=NODE_IP
+NODE_HOST_LIST[6]=NODE_IP
+NODE_HOST_LIST[7]=NODE_IP
+
+export NODE_HOSTS_STRING=${NODE_HOST_LIST[@]}
+
+export NUM_NODE=${#NODE_HOST_LIST[@]}
+
+# set up cluster-setting
+export NUM_WORKER=32
+export NUM_SERVER=8
+
+export NUM_WORKER_PER_NODE=`expr $NUM_WORKER / $NUM_NODE`
+export NUM_SERVER_PER_NODE=`expr $NUM_SERVER / $NUM_NODE`
+
+export DEBUG_PRINT=0
+export DEBUG_C_PRINT=0
+
+# set up network
+export DOCKER_IMAGE="public.ecr.aws/bdsldocker/ooo_backprop_data_parallel:latest"
+export SSH_KEY_PATH="SSH_KEY_PATH"
+export SSH_ID="ACCOUNT_ID"
+export DMLC_INTERFACE="NETWORK INTERFACE"
+
+../../expr/data-parallel/scripts/multi_node/run.sh preset

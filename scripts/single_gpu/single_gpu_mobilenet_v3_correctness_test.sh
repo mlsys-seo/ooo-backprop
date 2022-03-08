00a3b026d0bae69bfaf939e6def2c6ee6311e510
92hyungjun <78071764+92hyungjun@users.noreply.github.com>
2022-03-08 20:04:03 +0900

    Update single_gpu_mobilenet_v3_correctness_test.sh

--- scripts/single_gpu/single_gpu_mobilenet_v3_correctness_test.sh
+++ scripts/single_gpu/single_gpu_mobilenet_v3_correctness_test.sh
@@ -2,6 +2,8 @@
 
 DOCKER_IMAGE="public.ecr.aws/bdsldocker/ooo_backprop_single_gpu:latest"
 
+sudo docker pull $DOCKER_IMAGE
+
 echo "Running the single-GPU expr container..."
 docker run --rm --gpus all \
     ${DOCKER_IMAGE} \

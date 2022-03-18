bf808725373f08083c9a2006a122eb84d0d4e323
92hyungjun <78071764+92hyungjun@users.noreply.github.com>
2022-03-18 14:08:14 +0900

    Create logit_diff.py

---
+++ expr/pipe_par/correct_test/logit_diff.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def get_logit_diff():
+    logit_base = np.load("./logit_base.npy")
+    logit_ooo = np.load("./logit_ooo.npy")
+
+    print(f"logit_base : {logit_base}")
+    print(f"logit_ooo : {logit_ooo}")
+
+    logit_diff = logit_base - logit_ooo
+    avg_logit_diff = np.mean(logit_diff)
+    
+    print(f"logit_diff : {logit_diff}")
+
+
+if __name__ == "__main__":
+    get_logit_diff()
